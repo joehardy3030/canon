ccfe544ce7680ab7bdd0fd420f0927ea907059d7
Joe <jlhardyphd@gmail.com>
2019-12-08 19:11:44 -0800

    added objects to collect and start and end points

---
+++ Assets/Scripts/CollisionWithPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionWithPlayer : MonoBehaviour
+{
+    int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter (Collision collision)
+    {
+        print("Collided with " + collision.collider.gameObject.tag);
+        if (collision.collider.gameObject.tag == "pick_me")
+        {
+            Destroy(collision.collider.gameObject);
+            score++;
+            print("Score" +score);
+        }
+
+    }
+} 
