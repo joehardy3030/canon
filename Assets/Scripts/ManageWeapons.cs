f86bc9f9ebabbfd77f5799fb6c8f95359c2f231a
Joe <jlhardyphd@gmail.com>
2020-01-18 13:56:19 -0800

    reset the player to window and got a debug raycaster working; also put up some walls

---
+++ Assets/Scripts/ManageWeapons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageWeapons : MonoBehaviour
+{
+    Camera playersCamera;
+    Ray rayFromPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playersCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rayFromPlayer = playersCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        Debug.DrawRay(rayFromPlayer.origin, rayFromPlayer.direction * 100, Color.red);
+        //Debug.Log("Update");
+    }
+}
